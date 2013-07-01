2bec0fc6efb45942b2f964ccd502c74c84d3f52d
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2013-07-01 18:07:10 -0500

    mta1

---
+++ mta1/src/poo/ejer01/Mascota.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.ejer01;
+
+/**
+ * Desarrollar una clase mascota que tenga los
+ * atributos correctos desde el punto de vista
+ * de una veterinaria. Deberá tener un método
+ * llamado estaVacunado() que indique si la mascota
+ * está vacunada o no.
+ * Deberá devolver true si está vacunado y false si no está vacunado.
+ * @author heraldo
+ */
+public class Mascota {
+    
+}
---
+++ mta1/src/poo/ejer02/Dado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.ejer02;
+
+/**
+ * Desarrollar una clase llamada Dado
+ * que permita lanzar un dado que de forma aleatoria
+ * me devolverá un número entre el 1 al 6.
+ * El atributo principal será el valor actual
+ * y un método generará el número aleatorio.
+ * El sistema deberá mostrar el número resultado del dado. 
+ * @author heraldo
+ */
+public class Dado {
+    
+}
